10b2922508fea05591c14bb3e182e4faae4fb512
qeqqe <armansingh3172006@gmail.com>
2025-03-23 15:25:05 +0530

    Add Todo controller and all endpoints

--- src/main/java/com/example/todo_api/controller/TodoController.java
+++ src/main/java/com/example/todo_api/controller/TodoController.java
@@ -1,5 +1,71 @@
 package com.example.todo_api.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.todo_api.TodoService;
+import com.example.todo_api.model.dto.TodoRequest;
+import com.example.todo_api.model.dto.TodoResponse;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/todos")
 public class TodoController {
+    private final TodoService todoService;
+
+    @Autowired
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TodoResponse>> getAllTodos(@RequestParam(value = "completed", required = false) Boolean completed) {
+        List<TodoResponse> todos;
+        
+        if (completed != null) {
+            todos = todoService.getTodosByStatus(completed);
+        } else {
+            todos = todoService.getAllTodos();
+        }
+        
+        return ResponseEntity.ok(todos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TodoResponse> getTodoById(@PathVariable Long id) {
+        TodoResponse todo = todoService.getTodoById(id);
+        return ResponseEntity.ok(todo);
+    }
+
+    @PostMapping
+    public ResponseEntity<TodoResponse> createTodo(@Valid @RequestBody TodoRequest todoRequest) {
+        TodoResponse createdTodo = todoService.createTodo(todoRequest);
+        return new ResponseEntity<>(createdTodo, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<TodoResponse> updateTodo(@PathVariable Long id, @Valid @RequestBody TodoRequest todoRequest) {
+        TodoResponse updatedTodo = todoService.updateTodo(id, todoRequest);
+        return ResponseEntity.ok(updatedTodo);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delteTodo(@PathVariable Long id) {
+        todoService.deleteTodo(id);
+        return ResponseEntity.noContent().build();
+    }
+    
     
 }
